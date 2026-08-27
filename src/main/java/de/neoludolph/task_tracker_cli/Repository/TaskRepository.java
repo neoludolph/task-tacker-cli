6b5801717cc520d11db3c7be5adf012b780a607e
Neo Ludolph <neocan20@gmail.com>
2026-08-27 09:26:58 +0200

    feat: implement readFileAsString method in TaskRepository and add tasks.json file

--- src/main/java/de/neoludolph/task_tracker_cli/Repository/TaskRepository.java
+++ src/main/java/de/neoludolph/task_tracker_cli/Repository/TaskRepository.java
@@ -5,13 +5,13 @@ import java.nio.file.Paths;
 
 public class TaskRepository {
 
+    public static String readFileAsString(String file)throws Exception {
+        return new String(Files.readAllBytes(Paths.get(file)));
+    }
+
     public void loadTasksJson() throws Exception {
         String file = "src/main/resources/tasks.json";
         String json = readFileAsString(file);
-    }
-
-    public static String readFileAsString(String file)throws Exception {
-        return new String(Files.readAllBytes(Paths.get(file)));
     }
 
     public void saveTasksJson() {

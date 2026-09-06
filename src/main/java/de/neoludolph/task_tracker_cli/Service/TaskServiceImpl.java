84e69536a60119fe9c7271682076d18bd308fb47
Neo Ludolph <neocan20@gmail.com>
2026-09-06 13:21:27 +0200

    update add method to assign unique task IDs based on existing tasks

--- src/main/java/de/neoludolph/task_tracker_cli/Service/TaskServiceImpl.java
+++ src/main/java/de/neoludolph/task_tracker_cli/Service/TaskServiceImpl.java
@@ -5,6 +5,7 @@ import de.neoludolph.task_tracker_cli.Repository.TaskRepository;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskServiceImpl implements TaskService {
@@ -17,14 +18,23 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public void add(String description) {
+        TaskModel taskModel = new TaskModel(description);
         try {
             if (taskRepository.loadTasksJson().equals("[\n]")) {
-                TaskModel taskModel = new TaskModel(description);
                 taskModel.setId(0);
                 taskRepository.saveTasksJson(taskModel);
             } else {
                 String currentJson = taskRepository.loadTasksJson();
-                Pattern pattern = Pattern.compile("\"id\":\\s*(\\d+)");
+                Pattern pattern = Pattern.compile("\"id\":\\s*(\\d+)"); // \\s* -> null oder mehr Whitespaces, () -> Capturing Group, um alles darin gezielt auszulesen, \\d+ -> Mindestens eine Ziffer von 0-9
+                Matcher matcher = pattern.matcher(currentJson);
+
+                int lastId = 0;
+
+                while (matcher.find()) {
+                    lastId = Integer.parseInt(matcher.group(1));
+                }
+                taskModel.setId(lastId + 1);
+                taskRepository.saveTasksJson(taskModel);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
